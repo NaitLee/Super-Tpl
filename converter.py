1e651a8a75ac08ce2ca2afdf0ff7f4b762fdfe2e
NaitLee <naitli@foxmail.com>
2021-04-25 21:45:19 +0800

    Add files via upload

--- converter.py
+++ converter.py
@@ -1,28 +1,32 @@
 import os, sys, urllib.parse, base64
 
-folder = os.path.abspath(sys.argv[1])
+def convert(dirname = './'):
+    folder = os.path.abspath(dirname)
 
-files =filter(lambda x: os.path.isfile(folder + '/' + x) and x.startswith('~'), os.listdir(folder))
+    files = filter(lambda x: os.path.isfile(folder + '/' + x) and x.startswith('~'), os.listdir(folder))
 
-template = []
+    template = []
 
-for i in files:
-    f = open(folder + '/' + i, 'r', encoding='utf-8')
-    content = f.read()
-    f.close()
-    if i.endswith('.ts'):
-        b64 = base64.b64encode(content.encode('utf-8')).decode('utf-8')
-        os.system(f'node -e "let ts = require(\'./typescriptServices\'); console.log(ts.transpile(Buffer.from(\'{b64}\', \'base64\').toString(\'utf8\')));">.temp.js')
-        f = open('.temp.js', 'r', encoding='utf-8')
-        js = f.read()
+    for i in files:
+        f = open(folder + '/' + i, 'r', encoding='utf-8')
+        content = f.read()
         f.close()
-        os.remove('.temp.js')
-        content = js
-    section = '.'.join(urllib.parse.unquote(i).split('.')[0:-1])[1:]
-    template.append(f'\n[{section}]\n{content}\n')
+        if i.endswith('.ts'):
+            b64 = base64.b64encode(content.encode('utf-8')).decode('utf-8')
+            os.system(f'node -e "let ts = require(\'./typescriptServices\'); console.log(ts.transpile(Buffer.from(\'{b64}\', \'base64\').toString(\'utf8\')));">.temp.js')
+            f = open('.temp.js', 'r', encoding='utf-8')
+            js = f.read()
+            f.close()
+            os.remove('.temp.js')
+            content = js
+        section = '.'.join(urllib.parse.unquote(i).split('.')[0:-1])[1:]
+        template.append(f'\n[{section}]\n{content}\n')
 
-result = ''.join(template)
+    result = ''.join(template)
 
-f = open('output.tpl', 'w', encoding='utf-8')
-f.write(result)
-f.close()
+    f = open('output.tpl', 'w', encoding='utf-8')
+    f.write(result)
+    f.close()
+
+if __name__ == '__main__':
+    convert(sys.argv[1])
---
+++ watchdog.py
@@ -0,0 +1,37 @@
+import os, sys, time, datetime, converter
+
+class Watchdog():
+    def __init__(self, path_of_files):
+        self.path_of_files = path_of_files
+        fileset = set()
+        for i, j, k in os.walk(path_of_files):
+            for l in k:
+                newpath = os.path.join(i, l)
+                fileset.add(newpath)
+        self.files = list(fileset)
+        self.oldstats = {}
+        for i in self.files:
+            self.oldstats[i] = os.stat(i).st_mtime
+    def watch(self):
+        time.sleep(1.0)
+        newstats = {}
+        for i in self.files:
+            newstats[i] = os.stat(i).st_mtime
+        for i in newstats:
+            if newstats[i] != self.oldstats[i]:
+                self.bark()
+                break
+        self.oldstats = newstats
+    def bark(self):
+        current_time = datetime.datetime.now()
+        print(f'[{current_time}] Woof~ Building template...', end=' \t', flush=True)
+        converter.convert(self.path_of_files)
+        print('Done!')
+
+if __name__ == '__main__':
+    watchdog = Watchdog(sys.argv[1])
+    try:
+        while True:
+            watchdog.watch()
+    except KeyboardInterrupt:
+        pass
